b398da61b086795dc0f52ce3f36b27c268ca8e17
YuqiZhu29 <Zhuyuqi!521>
2021-01-05 17:23:37 -0800

    setup mako and basic web page

---
+++ video/app/libs/base_render.py
@@ -0,0 +1,34 @@
+# init mako
+
+from mako.lookup import TemplateLookup
+from django.template import RequestContext
+from django.conf import settings
+from django.template.context import Context
+from django.http import HttpResponse
+
+
+def render_to_response(request, template, data=None):
+    context_instance = RequestContext(request)
+    path = settings.TEMPLATES[0]['DIRS'][0]
+    lookup = TemplateLookup(
+        directories=path,
+    )
+
+    mako_template = lookup.get_template(template)
+
+    if not data:
+        data = {}
+
+    if context_instance:
+        context_instance.update(data)
+    else:
+        context_instance = Context(data)
+
+    result = {}
+
+    for d in context_instance:
+        result.update(d)
+
+    result['csrf_token'] = f'<input type="hidden" name="csrfmiddlewaretoken" value={request.META["CSRF_COOKIE"]}>'
+
+    return HttpResponse(mako_template.render(**result))
